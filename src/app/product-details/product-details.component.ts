d308c309770f3ea23b04b8380015bf463360dd96
hamo123-vi <amil.valjevac@stu.ibu.edu.ba>
2021-02-09 20:42:30 +0100

    managing_data_09022021_end

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { products } from '../products';
+import { CartService } from '../cart.service';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
@@ -10,12 +11,19 @@ export class ProductDetailsComponent implements OnInit {
   proizvod;
   constructor(
     private route: ActivatedRoute,
+    private cartService:CartService,
   ) { }
 
   ngOnInit() {
     const routeParams=this.route.snapshot.paramMap;
     const productIdFromRoute=Number(routeParams.get('productId'));
     this.proizvod=products.find(proizvod => proizvod.id === productIdFromRoute);
+  }
+
+  addToCart(proizvod)
+  {
+     this.cartService.addToCart(proizvod);
+     window.alert("Item is added to cart!!!");
   }
 
 }

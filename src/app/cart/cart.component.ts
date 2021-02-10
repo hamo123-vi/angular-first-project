75d9083e174ba99e84a68152f529906759bf39fb
hamo123-vi <amil.valjevac@stu.ibu.edu.ba>
2021-02-10 19:40:53 +0100

    Form Handling 10022021END

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -7,11 +8,27 @@ import { CartService } from '../cart.service';
 })
 export class CartComponent implements OnInit {
   items= this.cartService.getItems();
+  cartForm=this.formBuilder.group
+  (
+    {
+      name: '',
+      surname: '',
+      address: ''
+    }
+  )
   constructor(
     private cartService: CartService,
+    private formBuilder:FormBuilder
   ) { }
 
   ngOnInit() {
+  }
+
+  submit(): void
+  {
+    this.cartService.clearCart();
+    console.warn("Your data have been submitted! ",this.cartForm.value);
+    this.cartForm.reset();
   }
 
 }
